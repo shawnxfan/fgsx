67f48be7f5f16ec317ae1bdb4bccd322a95b0efd
FanXiang <flyandfanny@gmail.com>
2013-06-06 16:54:28 +0800

    控制台输出

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <QCoreApplication>
+#include <QTextStream>
 #include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    std::cout<<"nihao!!";
+    QTextStream cin(stdin, QIODevice::ReadOnly);
+    QTextStream cout(stdout, QIODevice::WriteOnly);
+    QTextStream cerr(stderr, QIODevice::WriteOnly);
+
+    cout<<"nihao!!"<<endl;
     return a.exec();
 }
