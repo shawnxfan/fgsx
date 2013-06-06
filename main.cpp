a3975a49e219be0a7f315666a301d23f28827f36
FanXiang <flyandfanny@gmail.com>
2013-06-06 16:37:14 +0800

    heh

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    //hello
+
+    std::cout<<"nihao";
     return a.exec();
 }
