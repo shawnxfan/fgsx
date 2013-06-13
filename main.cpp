f8de88d30dba80ed07b849d30fa007f418ed40b8
范翔 <fanxiang@FanXiang-Mac.local>
2013-06-13 09:45:09 +0800

    try

--- main.cpp
+++ main.cpp
@@ -1,17 +1,35 @@
 #include <QCoreApplication>
 #include <Client.h>
+#include <QThread>
+
+/*
+void rthread(void)
+{
+    client->ReadMesg(socketfd,buf,len);
+}
+
+static void* rthread_callback(void *) {
+    rthread();
+}
+
+void sthread(void)
+{
+    client->SendMesg(socketfd,buf);
+}
+
+static void* sthread_callback(void *) {
+    sthread();
+}*/
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-
+//    QCoreApplication a(argc, argv);
+    Client* client = new Client();
     struct sockaddr_in servaddr;
     char buf[256];
     int socketfd;
     int connstate;
     int len;
-    Client* client = new Client();
-
     //client->ConnectServer();
     memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
@@ -30,19 +48,34 @@ int main(int argc, char *argv[])
     tid = fork();
     if(tid == 0)
     {
-        while(true)
-        {
+        /*pthread_t rid;
+        int rret;
+        rret = pthread_create(&rid, NULL, rthread_callback, NULL);
+        if(rret != 0){
+            printf ("Create pthread error!n");
+            exit (1);
+        }
+        pthread_join(rid,NULL);*/
+        while(true){
             client->ReadMesg(socketfd,buf,len);
         }
     }
     else
     {
-        while(true)
-        {
+        /*pthread_t sid;
+        int sret;
+        sret = pthread_create(&sid, NULL, sthread_callback, NULL);
+        if(sret != 0){
+            printf ("Create pthread error!n");
+            exit (1);
+        }
+        pthread_join(sid,NULL);*/
+        while(true){
             client->SendMesg(socketfd,buf);
         }
         close(socketfd);
     }
-    return a.exec();
+    exit(1);
+//    return a.exec();
     return 0;
 }
